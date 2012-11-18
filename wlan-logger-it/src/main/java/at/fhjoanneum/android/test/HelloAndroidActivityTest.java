667f34035a2b864fba4fa155925225534d5372f9
Senshi <darksenshi@gmail.com>
2012-11-18 18:14:39 +0100

    initial commit, created project with maven command. no eclipse files yet.

---
+++ wlan-logger-it/src/main/java/at/fhjoanneum/android/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package at.fhjoanneum.android.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import at.fhjoanneum.android.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
